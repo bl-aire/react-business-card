eb29d860064f4d8e0e5a936ca68fa34b22ca7b7c
Blaire <ogoh_blessing@yahoo.com>
2022-09-03 18:21:10 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Info from './info';
+import About from './about';
+import Interest from './interest';
+import Footer from './footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <About />
+      <Interest />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
