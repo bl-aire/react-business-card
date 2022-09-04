195719dad53bf4193b361e17b533fca7d26f3cb9
Blaire <ogoh_blessing@yahoo.com>
2022-09-04 15:55:19 +0100

    Style component

--- src/components/info.js
+++ src/components/info.js
@@ -1,14 +1,13 @@
+import logo from '../../src/logo.svg'
 
 export default function Info() {
     return (
         <div className="info">
-            <img />
-            <h3>Ogoh Blessing</h3>
-            <p>Fontend Developer</p>
+            <img src={logo} alt="avatar" />
+            <h2 className='name'>Ogoh Blessing</h2>
+            <p className="role">Fontend Developer</p>
             <small>ogoh_blessing@yahoo.com</small>
             <button>Email</button>
         </div>
-       
-
     )
 }
