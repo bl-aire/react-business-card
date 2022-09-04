8a68157b5c9d8cfb303346d974a0f3abe1fb19c8
Blaire <ogoh_blessing@yahoo.com>
2022-09-04 16:21:51 +0100

    Add avatar

--- src/components/info.js
+++ src/components/info.js
@@ -1,9 +1,10 @@
 import logo from '../../src/logo.svg'
+import avatar from '../../src/avatar.png'
 
 export default function Info() {
     return (
         <div className="info">
-            <img src={logo} alt="avatar" />
+            <img src={avatar} alt="avatar" />
             <h2 className='name'>Ogoh Blessing</h2>
             <p className="role">Fontend Developer</p>
             <small>ogoh_blessing@yahoo.com</small>
